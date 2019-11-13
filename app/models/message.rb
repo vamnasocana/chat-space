28540ee56c3a6c78d2c9096b381fd50b2620b025
vamnasocana <vamnasocana@gmail.com>
2019-11-13 22:33:13 +0900

    send_messages_step5
    メッセージの送受信機能

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,6 @@
 class Message < ApplicationRecord
-  belongs_to: user
-  belongs_to: group
+  belongs_to :user
+  belongs_to :group
 
   validates :body, presence: true, unless: :image?
   mount_uploader :image, ImageUploader
