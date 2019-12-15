c88b8f7d6db4d0d00589c44199fc06bd6ccb7510
vamnasocana <vamnasocana@gmail.com>
2019-12-15 16:55:40 +0900

    Capistrano2

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,8 @@ set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/key_pem.pem'] 
+                  keys: ['~/.ssh/vamnasocana.pem'] 
+                  # keys: ['~/.ssh/key_pem.pem'] 
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
